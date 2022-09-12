3c5067411d062888c591fc10d115a36804af9522
Calista Vimalametta <calista.vimalametta@ui.ac.id>
2022-09-12 11:27:04 +0700

    Update Tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+# import untuk mengambil data dari database
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+# membuat fungsi menampilkan katalog
+def show_katalog(request):
+    # menambahkan context untuk ditampilkan
+    data_item_katalog = CatalogItem.objects.all()
+    context = {
+        'list_item': data_item_katalog,
+        'nama': 'Calista Vimalametta Heryadi',
+        'npm': '2106630473'
+    }
+    return render(request, "katalog.html", context)
